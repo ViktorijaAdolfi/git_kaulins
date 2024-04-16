f46028c6994f554419cf818009fe9ec0135a0032
Adolfi <viktorija.adolfi@lvt.sk.lv>
2024-04-16 11:44:09 +0300

    Projekta pirmā versija

--- src/git_kaulins/kaulins.java
+++ src/git_kaulins/kaulins.java
@@ -1,10 +1,14 @@
 package git_kaulins;
 
+import java.util.Random;
+
 public class kaulins {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int skaitlis;
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6)+1;
+		System.out.println("Tu uzmeti: "+skaitlis);
 	}
 
 }

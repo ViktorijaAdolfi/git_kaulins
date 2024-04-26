1a1cde0cd9bd33c098952e5441bf824ca33df810
Viktorija <viktorijadolfi@gmail.com>
2024-04-26 08:16:06 +0300

    pievienots ierobežojums ievadei

--- src/git_kaulins/kaulins.java
+++ src/git_kaulins/kaulins.java
@@ -3,12 +3,15 @@ package git_kaulins;
 import java.util.Random;
 
 public class kaulins {
-
-	public static void main(String[] args) {
+	static void mestKaulinu() {
 		int skaitlis;
 		Random rand = new Random();
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Tu uzmeti: "+skaitlis);
+	}
+
+	public static void main(String[] args) {
+		mestKaulinu();
 	}
 
 }
